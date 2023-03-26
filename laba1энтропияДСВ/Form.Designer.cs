f2150347aa67ea1cc17addf0cffe82c04350a850
Uliay8 <ulanavorobeva130@gmail.com>
2023-03-26 22:47:21 +0500

    Смотрю как работает с VS

---
+++ laba1энтропияДСВ/Form.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace laba1энтропияДСВ
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonResoult = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResoult = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(281, 225);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество событий:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(173, 7);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 21);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // buttonResoult
+            // 
+            this.buttonResoult.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonResoult.Location = new System.Drawing.Point(299, 205);
+            this.buttonResoult.Name = "buttonResoult";
+            this.buttonResoult.Size = new System.Drawing.Size(176, 30);
+            this.buttonResoult.TabIndex = 3;
+            this.buttonResoult.Text = "Вычислить энтропию";
+            this.buttonResoult.UseVisualStyleBackColor = false;
+            this.buttonResoult.Click += new System.EventHandler(this.buttonResoult_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(296, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Энтропия:";
+            // 
+            // labelResoult
+            // 
+            this.labelResoult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelResoult.Location = new System.Drawing.Point(374, 238);
+            this.labelResoult.Name = "labelResoult";
+            this.labelResoult.Size = new System.Drawing.Size(98, 25);
+            this.labelResoult.TabIndex = 5;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(299, 38);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(176, 28);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(299, 72);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(176, 28);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Управление:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 275);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.labelResoult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonResoult);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Энтропия ДСВ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonResoult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResoult;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ laba1энтропияДСВ/Form.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1энтропияДСВ
+{
+    public partial class MainForm : Form
+    {
+        private int n;
+        private List<double> probabilites = new List<double>();
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        private bool isUnique;
+        private string prevValue;
+        private bool isSumCorrect = false;
+        //++++ввели количество элементов
+        //ввели вероятности - надо проверить что каждое значение от 0 до 1
+        // сумма не больше 1,
+        // при вычислении не меньше единицы + все значения введены
+        // сразу сообщение
+        //отметка сортировки
+        //кнопки
+        public MainForm()
+        {
+            InitializeComponent();
+            ds.Tables.Add(dt);
+            DataColumn eventX = new DataColumn("Событие", Type.GetType("System.String"));
+            ds.Tables[0].Columns.Add(eventX);
+            DataColumn probability = new DataColumn("Вероятность", Type.GetType("System.String"));
+            ds.Tables[0].Columns.Add(probability);
+            dataGridView1.DataSource = ds.Tables[0];
+            dataGridView1.Columns[0].SortMode = DataGridViewColumnSortMode.Automatic;
+            CreateRow(1);
+            //checkedListBox1.SetItemChecked(0, true);
+        }
+        private void CreateRow(int i)
+        {
+            DataRow row = dt.NewRow();
+            for (int j = 0; j <= i; j++)
+            {
+                if (CheckUniqueness((j + 1).ToString(), i))
+                {
+                    row["Событие"] = (j + 1).ToString();
+                    ds.Tables[0].Rows.Add(row);
+                    break;
+                }
+            }
+            //проверка на пристствующие числа, если какого-то нет то и=оно
+        }
+        private bool CheckUniqueness (string title, int index)
+        {
+            isUnique = true;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if ((ds.Tables[0].Rows[i].ItemArray[0].ToString() == title) && (i != index))
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+            return isUnique;
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(numericUpDown1.Value);
+            if (n > ds.Tables[0].Rows.Count)
+            {
+                for (int i = ds.Tables[0].Rows.Count; i < n; i++)
+                {
+                    CreateRow(i);
+                }
+            }
+            else
+            {
+                while (ds.Tables[0].Rows.Count != n)
+                {
+                    ds.Tables[0].Rows.RemoveAt(ds.Tables[0].Rows.Count - 1);
+                }
+            }
+            labelResoult.Text = "";
+        }
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            prevValue = dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString();
+        }
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (Convert.ToInt32(e.ColumnIndex) == 0)
+            {
+                if (!CheckUniqueness(dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString(), e.RowIndex))
+                {
+                    dataGridView1[e.ColumnIndex, e.RowIndex].Value = prevValue;
+                    MessageBox.Show("Значения событий должны быть уникальными!");
+                }
+            }
+            if (Convert.ToInt32(e.ColumnIndex) == 1)
+            {
+                
+                if (dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString().Any(c => c == '.'))
+                {
+                    MessageBox.Show("Введите значение через запятую");
+                    dataGridView1[e.ColumnIndex, e.RowIndex].Value = "";
+                }
+                else
+                {
+                    try
+                    {
+                        if (dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString().Any(c => char.IsLetter(c)) ||
+                        Convert.ToDouble(dataGridView1[e.ColumnIndex, e.RowIndex].Value) > 1 ||
+                        Convert.ToDouble(dataGridView1[e.ColumnIndex, e.RowIndex].Value) < 0)
+                        {
+                            dataGridView1[e.ColumnIndex, e.RowIndex].Value = "";
+                            MessageBox.Show("Значение вероятности должно принадлежать промежутку [0;1]");
+                        }
+                        else
+                        {
+                            if (!SumOfProbabilites())
+                                dataGridView1[e.ColumnIndex, e.RowIndex].Value = "";
+                        }
+                    }
+                    catch (FormatException) {}
+                }
+                labelResoult.Text = "";
+            }
+        }
+        private bool SumOfProbabilites()
+        {
+            double probability = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value.ToString() != "")
+                    probability += Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+            }
+            probability = Math.Round(probability, 3);
+            if (probability > 1)
+            {
+                MessageBox.Show("Сумма вероятностей должна быть не больше единицы");
+                return false;
+            }
+            if (probability < 1) isSumCorrect = false;
+            else isSumCorrect = true;
+            return true;
+        }
+
+        private void buttonResoult_Click(object sender, EventArgs e)
+        {
+            bool allValues = true;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value.ToString() == "")
+                {
+                    MessageBox.Show($"Введите значение в {i+1} строчке (событие \"{dataGridView1.Rows[i].Cells[0].Value}\")");
+                    allValues = false;
+                    break;
+                }
+            }
+            if (allValues)
+            {
+                if (isSumCorrect)
+                {
+                    double res = Shennon();
+                    labelResoult.Text = res.ToString();
+                }
+                else MessageBox.Show("Сумма вероятностей должна быть равна единице");
+            }
+            
+        }
+        private double Shennon()
+        {
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value) != 0.0)
+                {
+                    double x = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                    sum -= x * Math.Log(x, 2);
+                }
+            }
+            return Math.Round(sum, 3);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                ds.Tables[0].Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                if (numericUpDown1.Value == 1)
+                {
+                    ds.Tables[0].Clear();
+                    CreateRow(1);
+                }
+                else numericUpDown1.Value -= 1;
+                dataGridView1.CurrentCell = null;
+                SumOfProbabilites();
+            }
+            else MessageBox.Show("Выберите ячейку, ряд которой нужно удалить");
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            ds.Tables[0].Clear();
+            if (numericUpDown1.Value == 1)
+                CreateRow(1);
+            else numericUpDown1.Value = 1;
+        }
+
+        //private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        //{
+        //    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+        //    {
+        //        if (i != e.Index)
+        //            checkedListBox1.SetItemChecked(i, false);
+        //    }
+        //    if (e.Index == 1)
+        //        dataGridView1.Sort()
+        //    if (e.Index == 2) 
+        //        BubbleSort(1);
+        //}
+        //private void BubbleSort(int index)
+        //{
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = i + 1; j < n; j++)
+        //        {
+        //            if (ds.Tables[0].Rows[i].ItemArray[index].ToString().CompareTo(ds.Tables[0].Rows[j].ItemArray[index].ToString()) > 0)
+        //            {
+        //                //string str = ds.Tables[0].Rows[i].ItemArray[0].ToString();
+        //                //string str2 = ds.Tables[0].Rows[i].ItemArray[1].ToString();
+        //                //ds.Tables[0].Rows[i].ItemArray[0] = "";
+        //                //ds.Tables[0].Rows[i].ItemArray[1] = "";
+        //                //string str11 = ds.Tables[0].Rows[j].ItemArray[0].ToString();
+        //                //string str12 = ds.Tables[0].Rows[j].ItemArray[1].ToString();
+        //                //ds.Tables[0].Rows[j].ItemArray[0] = str;
+        //                //ds.Tables[0].Rows[j].ItemArray[1] = str2;
+        //                //ds.Tables[0].Rows[i].ItemArray[0] = str11;
+        //                //ds.Tables[0].Rows[i].ItemArray[1] = str12;
+                         
+        //                //DataRow row1 = ds.Tables[0].Rows[i];
+        //                //DataRow row2 = ds.Tables[0].Rows[j];
+        //                //ds.Tables[0].Rows.RemoveAt(j);
+        //                //ds.Tables[0].Rows.RemoveAt(i);
+        //                //ds.Tables[0].Rows.InsertAt(row2, i);
+        //                //ds.Tables[0].Rows.InsertAt(row1, j);
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
